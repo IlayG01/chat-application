75e1da4139e05e6e7ab51ce08ba62b721678be9d
Ilay Gilman <ilaygilman@gmail.com>
2021-03-08 11:34:01 +0200

    Adding basic authentication

--- app.py
+++ app.py
@@ -1,22 +1,30 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_socketio import SocketIO, join_room, leave_room
+from flask_login import LoginManager, login_user, current_user, login_required, logout_user
+from db import get_user
 
 app = Flask(__name__)
-
+app.secret_key = 'my-secret-key'
 socketio = SocketIO(app)
+login_manager = LoginManager()
+login_manager.login_view = 'login'
+login_manager.init_app(app)
 
 
+# TODO - move routes, socketio, login into seperate files
 @app.route("/")
 def base():
-    return redirect(url_for('home'))
+    return redirect(url_for('login'))
 
 
 @app.route('/home')
+@login_required
 def home():
     return render_template('index.html')
 
 
 @app.route('/chat')
+@login_required
 def chat():
     username = request.args.get('username')
     room_id = request.args.get('room_id')
@@ -26,6 +34,32 @@ def chat():
         return redirect(url_for('home'))
 
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+
+    message = ''
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        user = get_user(username)
+        if user and user.check_password(password):
+            login_user(user)
+            return redirect(url_for('home'))
+        else:
+            message = 'Failed to login'
+    return render_template('login.html', message=message)
+
+
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+
+
+### SOCKET-IO ###
 @socketio.on('join_room')
 def handle_join_room(data):
     app.logger.info(f'{data["username"]} joined {data["room_id"]} room')
@@ -44,6 +78,12 @@ def handle_leave_room(data):
 def handle_send_message(data):
     app.logger.info(f'{data["username"]} messaged:\n {data["message"]}')
     socketio.emit('message_announcement', data)
+
+
+### AUTHENTICATION ###
+@login_manager.user_loader
+def load_user(username):
+    return get_user(username)
 
 
 if __name__ == '__main__':
--- db.py
+++ db.py
@@ -1,3 +1,4 @@
+from user import User
 from pymongo import MongoClient
 from werkzeug.security import generate_password_hash
 
@@ -14,4 +15,10 @@ def save_user(username, email, password):
     })
 
 
-save_user("Josh", "josh@gmail.com", "josh")
+def get_user(username):
+    user = user_collection.find_one({
+        "_id": username
+    })
+    return User(user['_id'], user['email'], user['password']) or None
+
+# save_user("Josh", "josh@gmail.com", "josh")
